8201681431e3535f3e86dc85925384917709f073
Fadeshark <fadeevk1304@gmail.com>
2024-12-12 10:38:02 +0300

    5.py

---
+++ Pract/15 PRC/5.py
@@ -0,0 +1,12 @@
+import turtle
+
+
+turtle.fillcolor("red")
+turtle.begin_fill()
+for i in range(5):
+    turtle.forward(100)
+    turtle.left(72)
+    turtle.forward(100)
+    turtle.right(144)
+turtle.end_fill()
+turtle.done()
