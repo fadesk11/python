a2b471e4e0dba31c79c42db7b6d239373e176301
Fadeshark <fadeevk1304@gmail.com>
2024-12-16 15:57:31 +0300

    7.py

---
+++ Pract/15 PRC/7.py
@@ -0,0 +1,15 @@
+import turtle
+
+a = turtle.Turtle()
+b = turtle.Turtle()
+a.color("green")
+a.circle(100,180)
+a.home()
+b.color("red")
+b.left(180)
+b.circle(-100,180)
+b.left(180)
+b.circle(100,90)
+b.left(90)
+b.forward(200)
+turtle.done()
