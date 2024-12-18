22c25959f027ee0f05213b39bf542a6aaa1f25ed
Fadeshark <fadeevk1304@gmail.com>
2024-12-19 00:25:40 +0300

    9.py

---
+++ Pract/15 PRC/9.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.Screen().bgpic("back1.png")
+turtle.goto(0,-100)
+turtle.goto(-100,-100)
+turtle.goto(-100,100)
+turtle.goto(50,100)
+turtle.goto(50,50)
+turtle.goto(100,50)
+turtle.goto(100,-50)
+turtle.done()
